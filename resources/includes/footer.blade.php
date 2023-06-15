3f5359b73c2a911cb6a61cf09b08f70033f9e517
nikenayw <nikenwjy23@gmail.com>
2023-06-15 15:35:52 +0700

    submit

---
+++ resources/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
+                <h2 class="footer-heading"><a href="#" class="logo">Pinjam Ruang</a></h2>
+                <p>Sebuah website yang dapat meminjam ruangan secara mudah dan cepat </p>
+                <a href="#">Baca lebih lajut <span class="fa fa-chevron-right" style="font-size: 11px;"></span></a>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
+                <h2 class="footer-heading">Layanan</h2>
+                <ul class="list-unstyled">
