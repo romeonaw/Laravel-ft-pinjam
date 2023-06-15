e78e0ab790a24a98f377b3db0fcbdf1b9a62f023
nikenayw <nikenwjy23@gmail.com>
2023-06-15 15:40:20 +0700

    submit

--- resources/includes/footer.blade.php
+++ resources/includes/footer.blade.php
@@ -9,3 +9,55 @@
             <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
                 <h2 class="footer-heading">Layanan</h2>
                 <ul class="list-unstyled">
+                    <li><a href="https://registrasi.unsoed.ac.id/" target="_blank" class="py-1 d-block">Pendaftaran</a></li>
+                    <li><a href="https://sia.akademik.unsoed.ac.id/" target="_blank" class="py-1 d-block">Sistem Informasi Akademik</a></li>
+                    <li><a href="http://ft.unsoed.ac.id/?ac_gallery=perpus-1" target="_blank" class="py-1 d-block">Perpustakaan</a></li>
+                    <li><a href="http://ft.unsoed.ac.id/category/jurnal/" target="_blank" class="py-1 d-block">Jurnal</a></li>
+                </ul>            
+            </div>
+            <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
+                <h2 class="footer-heading">Tag</h2>
+                <div class="tagcloud">
+        <a href="#" class="tag-cloud-link">Fakultas Teknik</a>
+        <a href="#" class="tag-cloud-link">pinjam</a>
+        <a href="#" class="tag-cloud-link">ruang</a>
+        <a href="#" class="tag-cloud-link">unsoed</a>
+      </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
+                <h2 class="footer-heading">Langganan</h2>
+                <form action="#" class="subscribe-form">
+      <div class="form-group d-flex">
+        <input type="text" class="form-control rounded-left" placeholder="Enter email address">
+        <button type="submit" class="form-control submit rounded-right"><span class="sr-only">Submit</span><i class="fa fa-paper-plane"></i></button>
+      </div>
+    </form>
+    <h2 class="footer-heading mt-5">Ikuti kami</h2>
+    <ul class="ftco-footer-social p-0">
+     
+      {{-- <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="fa fa-facebook"></span></a></li> --}}
+      <li class="ftco-animate"><a href="https://www.instagram.com/blaterism/" target="_blank" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="fa fa-instagram"></span></a></li>
+    </ul>
+            </div>
+        </div>
+    </div>
+    <div class="w-100 mt-5 border-top py-5">
+        <div class="container">
+            <div class="row">
+      <div class="col-md-6 col-lg-8">
+
+        <p class="copyright mb-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |  <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://github.com/romeonaw/Laravel-ft-pinjam/tree/master" target="_blank">Uncode Team</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+      </div>
+      <div class="col-md-6 col-lg-4 text-md-right">
+          <p class="mb-0 list-unstyled">
+              <a class="mr-md-3" href="#">Ketentuan</a>
+              <a class="mr-md-3" href="#">Privasi</a>
+              
+          </p>
+      </div>
+    </div>
+        </div>
+    </div>
+</footer>
