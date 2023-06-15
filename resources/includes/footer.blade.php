abf71af0830b54e94a23b635508db00b2bda22db
nikenayw <nikenwjy23@gmail.com>
2023-06-15 15:57:25 +0700

    niken

--- resources/includes/footer.blade.php
+++ resources/includes/footer.blade.php
@@ -61,3 +61,28 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
         </div>
     </div>
 </footer>
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+<script src="{{ asset('vendor/technext/vacation-rental/js/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/jquery-migrate-3.0.1.min.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/popper.min.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/jquery.easing.1.3.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/jquery.stellar.min.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/jquery.animateNumber.min.js') }}"></script>
+{{-- <script src="{{ asset('vendor/technext/vacation-rental/js/bootstrap-datepicker.js') }}"></script> --}}
+{{-- <script src="{{ asset('vendor/technext/vacation-rental/js/jquery.timepicker.min.js') }}"></script> --}}
+<script src="{{ asset('vendor/technext/vacation-rental/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('vendor/technext/vacation-rental/js/scrollax.min.js') }}"></script>
+{{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> --}}
+{{-- <script src="{{ asset('vendor/technext/vacation-rental/js/google-map.js') }}"></script> --}}
+
+<!-- Datetime picker -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.39.0/js/tempusdominus-bootstrap-4.min.js" integrity="sha512-k6/Bkb8Fxf/c1Tkyl39yJwcOZ1P4cRrJu77p83zJjN2Z55prbFHxPs9vN7q3l3+tSMGPDdoH51AEU8Vgo1cgAA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="{{ asset('vendor/technext/vacation-rental/js/main.js') }}"></script>
